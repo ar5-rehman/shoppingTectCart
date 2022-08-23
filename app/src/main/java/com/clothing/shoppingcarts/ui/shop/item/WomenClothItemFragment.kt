0a20c76cfca0ea9b0f5d669980bcffdb0f3928d2
ar5-rehman <ark1111.khan@gmail.com>
2022-08-23 16:42:25 +0500

    testing circle ci

--- app/src/main/java/com/clothing/shoppingcarts/ui/shop/item/WomenClothItemFragment.kt
+++ app/src/main/java/com/clothing/shoppingcarts/ui/shop/item/WomenClothItemFragment.kt
@@ -124,7 +124,6 @@ class WomenClothItemFragment : BaseFragment(R.layout.fragment_women_cloth_item) 
                 buyListener(object: BuyListener{
                     override fun onBuy(index: Int) {
 
-                        
                         viewModel.tryObservable.set(false)
                         viewModel.buyObservable.set(true)
                         viewModel.addObservable.set(false)
